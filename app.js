7cfceb9710f9015a92f7a348288a0fea52388ca3
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-01-24 16:29:12 -0200

    projeto node

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+	res.send("<html><body>Porta de Noticias</body></html>");
+
+
+});
+
+app.get('/tecnologia',function(req,res){
+	res.send("<html><body>Porta de tecnologia</body></html>");
+
+
+});
+
+
+app.listen(3000, function(){
+
+	console.log("Servidor rodando com Express");
+
+});
+
+
+
+
---
+++ http.js
@@ -0,0 +1,24 @@
+var http = require('http');
+
+
+var server = http.createServer(function(req,res){
+	var categoria = req.url;
+
+	if(categoria=='/tecnologia'){
+		res.end("<html><body>Portal de tecnologia</body></html>");
+
+	}else if(categori'a=='/moda'){
+		res.end("<html><body>Portal de moda</body></html>");
+
+	}else if(categoria=='/beleza'){
+		res.end("<html><body>Portal de beleza</body></html>");
+
+	}else{
+		res.end("<html><body>Portal de noticias</body></html>");
+
+	}
+
+
+});
+
+server.listen(3000);
