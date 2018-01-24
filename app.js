ebd411bff50e7771213150313e48791f5acfab67
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-01-24 17:13:40 -0200

    trabalhando com views

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ var app = express();
 
 app.set('view engine','ejs');
 
+app.get('/tecnologia',function(req,res){
+	res.render("secao/tecnologia");
+});
+
 app.get('/',function(req,res){
 	res.send("<html><body>Porta de Noticias</body></html>");
 
 
 });
-
-app.get('/tecnologia',function(req,res){
-	res.send("<html><body>Porta de tecnologia</body></html>");
-
-
-});
-
 
 app.listen(3000, function(){
 
