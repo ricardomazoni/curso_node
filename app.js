5fed1ee810e84d43774c45230e365d51f4454e81
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-01-30 14:48:25 -0200

    adicionando projeto

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var app = require('./config/server');
 var rotaNoticias = require('./app/routes/noticias');
 rotaNoticias(app);
 
+var rotaHome = require('./app/routes/home')(app);
+
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia');
+rotaFormInclusaoNoticia(app);
+
+
 app.listen(3000, function(){
 
 	console.log('Servidor On');
